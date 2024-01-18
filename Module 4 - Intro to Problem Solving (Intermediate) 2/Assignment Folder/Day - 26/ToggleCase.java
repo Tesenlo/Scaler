51c1c7e025e2f8e420eb52eb4e028a50cd9b1a26
Tesenlo <132662818+Tesenlo@users.noreply.github.com>
2024-01-18 13:59:39 +0530

    Update ToggleCase.java

--- Module 4 - Intro to Problem Solving (Intermediate) 2/Assignment Folder/Day - 26/ToggleCase.java
+++ Module 4 - Intro to Problem Solving (Intermediate) 2/Assignment Folder/Day - 26/ToggleCase.java
@@ -1,1 +1,27 @@
+// Problem Description
+// You are given a character string A having length N, consisting of only lowercase and uppercase latin letters.
+// You have to toggle case of each character of string A. For e.g 'A' is changed to 'a', 'e' is changed to 'E', etc.
+
+public class Solution {
+
+    public static char toggle(char ch){
+        if(ch>='A' && ch<='Z'){
+            ch = (char) (ch+32);
+        }else{
+            ch = (char) (ch-32);
+        }
+        return ch;
+    }
+  
+    public String solve(String A) {
+        int n = A.length();
+        StringBuilder ans = new StringBuilder();
+
+        for(int i=0; i<n; i++){
+            ans.append(toggle(A.charAt(i)));
+        }
+        String str = ans.toString();
+        return str;
+    }
+}
 
